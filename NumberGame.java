c4e0365d3122b57b20a35e4fc5902ec370a1fd7d
kxnishk08 <143330973+kxnishk08@users.noreply.github.com>
2024-06-23 15:32:37 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,1 +1,27 @@
-
+import java.util.*;
+public class NumberGame{
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+        int min=0;
+        int x=0;
+        int max=100;
+        int num = (int)(Math.random()*(max-min+1)+min);  
+        System.out.println("Enter Your Guess");
+        while(x!=1){
+            int guess = sc.nextInt();
+            if(guess>num)
+            {
+                System.out.println("Too High");  
+            }
+            else if (guess==num)
+            {
+                System.out.println("You guessed the number!");
+                x++;
+            }
+            else if (guess<num)
+            {
+                System.out.println("Too Low");
+            }
+        }
+    }
+}
